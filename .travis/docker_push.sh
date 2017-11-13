612a08e686327986b96d984321551e629ab67274
Boris Skert <boris.skert@adorsys.de>
2017-11-13 11:19:52 +0100

    scripts and config for travis-ci

---
+++ .travis/docker_push.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+IMAGE_NAME="${DOCKER_IMAGE_NAME}:${TRAVIS_TAG}"
+
+docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
+docker build -t $IMAGE_NAME .
+docker push $IMAGE_NAME
