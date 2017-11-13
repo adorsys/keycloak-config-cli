cfbe3406a7c22b980e0bd0cc8c0e1eaf09a5b5fa
Boris Skert <boris.skert@adorsys.de>
2017-11-13 17:25:20 +0100

    build 'latest'-image; adjust README; clean up code

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -2,9 +2,12 @@
 set -e
 
 IMAGE_NAME="${DOCKER_IMAGE_NAME}:${TRAVIS_TAG}"
-
-ls -la ./target/*.jar
+LATEST_IMAGE_NAME="${DOCKER_IMAGE_NAME}:latest"
 
 docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
+
 docker build -t $IMAGE_NAME .
+docker tag $IMAGE_NAME $LATEST_IMAGE_NAME
+
 docker push $IMAGE_NAME
+docker push $LATEST_IMAGE_NAME
--- .travis/maven_deploy.sh
+++ .travis/maven_deploy.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-TAG_PATTERN="^v([[:digit:]]+\.)+[[:digit:]]+(-[[:digit:]]+)?$"
-
-if [[ ${TRAVIS_TAG} =~ ${TAG_PATTERN} ]]; then
-  echo "RELEASE TAG -> publish $TRAVIS_TAG to mvn central";
-  mvn --settings .travis/settings.xml package gpg:sign deploy -Prelease -DskipTests -B -U;
-else
-  echo "NO RELEASE TAG -> don't publish to mvn central";
-fi
+mvn --settings .travis/settings.xml package gpg:sign deploy -Prelease -DskipTests -B -U;
