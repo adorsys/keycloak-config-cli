2a74563d8259f8724b180326805caeab666143fd
tim.smyth <tim.smyth@kiwigrid.com>
2018-08-27 10:46:17 +0200

    Add roles to user

--- src/main/java/de/adorsys/keycloak/config/user/UserConfigService.java
+++ src/main/java/de/adorsys/keycloak/config/user/UserConfigService.java
@@ -1,8 +1,10 @@
 package de.adorsys.keycloak.config.user;
 
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.core.Response;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserConfigService {
@@ -41,11 +46,26 @@ public class UserConfigService {
 
         if (response.getStatus() < 400) {
             LOG.debug("Creating user '{}' in realm '{}'.", userRepresentation.getUsername(), realmId);
-            keycloak.proxy(UserResource.class, response.getLocation());
+			UserResource proxy = keycloak.proxy(UserResource.class, response.getLocation());
+			addRealmRolesToUser(realmId,userRepresentation,proxy);
         } else {
             throw new RuntimeException("Could not create user: " + userRepresentation.getUsername());
         }
 
         response.close();
     }
+
+	private void addRealmRolesToUser(String realmId, UserRepresentation userRepresentation,UserResource proxy){
+		LOG.info("Adding realm roles '{}'",userRepresentation.getRealmRoles());
+		List<RoleRepresentation> transformedRoles = transformRoles(keycloak.realm(realmId),
+				userRepresentation.getRealmRoles());
+		proxy.roles().realmLevel().add(transformedRoles);
+		LOG.info("Added {} roles to '{}'",transformedRoles.size(), userRepresentation.getUsername());
+
+	}
+
+	private List<RoleRepresentation> transformRoles(RealmResource realmResource, List<String> roles){
+		return roles.stream().map(role -> realmResource.roles()
+				.get(role).toRepresentation()).collect(Collectors.toCollection(ArrayList::new));
+	}
 }
