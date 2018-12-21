9eb6f38dd9d0690256d5c9f0d7778fc9c3b633a7
Boris Skert <boris.skert@adorsys.de>
2018-12-21 11:30:37 +0100

    introduce integration-tests for creation realms with a client

---
+++ config-cli/src/test/java/de/adorsys/keycloak/config/ImportClientsIT.java
@@ -0,0 +1,147 @@
+package de.adorsys.keycloak.config;
+
+import de.adorsys.keycloak.config.configuration.TestConfiguration;
+import de.adorsys.keycloak.config.model.KeycloakImport;
+import de.adorsys.keycloak.config.model.RealmImport;
+import de.adorsys.keycloak.config.service.KeycloakImportProvider;
+import de.adorsys.keycloak.config.service.KeycloakProvider;
+import de.adorsys.keycloak.config.service.RealmImportService;
+import de.adorsys.keycloak.config.util.ResourceLoader;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.common.util.MultivaluedHashMap;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.ComponentRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(
+        classes = { TestConfiguration.class },
+        initializers = { ConfigFileApplicationContextInitializer.class }
+)
+@ActiveProfiles("IT")
+@DirtiesContext
+public class ImportClientsIT {
+    private static final String REALM_NAME = "realmWithClients";
+
+    @Autowired
+    RealmImportService realmImportService;
+
+    @Autowired
+    KeycloakImportProvider keycloakImportProvider;
+
+    @Autowired
+    KeycloakProvider keycloakProvider;
+
+    KeycloakImport keycloakImport;
+
+    @Before
+    public void setup() throws Exception {
+        File configsFolder = ResourceLoader.loadResource("import-files/clients");
+        this.keycloakImport = keycloakImportProvider.readRealmImportsFromDirectory(configsFolder);
+    }
+
+    @After
+    public void cleanup() throws Exception {
+        keycloakProvider.close();
+    }
+
+    @Test
+    public void shouldReadImports() {
+        assertThat(keycloakImport, is(not(nullValue())));
+    }
+
+    @Test
+    public void integrationTests() throws Exception {
+        shouldCreateRealmWithClient();
+    }
+
+    private void shouldCreateRealmWithClient() throws Exception {
+        doImport("0_create_realm_with_client.json");
+
+        RealmRepresentation createdRealm = keycloakProvider.get().realm(REALM_NAME).toRepresentation();
+
+        assertThat(createdRealm.getRealm(), is(REALM_NAME));
+        assertThat(createdRealm.isEnabled(), is(true));
+
+        ClientRepresentation createdClient = getClient(
+                "moped-client"
+        );
+
+        assertThat(createdClient.getName(), is("moped-client"));
+        assertThat(createdClient.getClientId(), is("moped-client"));
+        assertThat(createdClient.getDescription(), is("Moped-Client"));
+        assertThat(createdClient.isEnabled(), is(true));
+        assertThat(createdClient.getClientAuthenticatorType(), is("client-secret"));
+        assertThat(createdClient.getRedirectUris(), is(containsInAnyOrder("*")));
+        assertThat(createdClient.getWebOrigins(), is(containsInAnyOrder("*")));
+
+        // client secret on this place is always null...
+        assertThat(createdClient.getSecret(), is(nullValue()));
+
+        // ... and has to be retrieved separately
+        String clientSecret = getClientSecret(createdClient.getId());
+        assertThat(clientSecret, is("my-special-client-secret"));
+    }
+
+    private ClientRepresentation getClient(String clientId) {
+        List<ClientRepresentation> foundClients = keycloakProvider.get()
+                .realm(REALM_NAME)
+                .clients()
+                .findByClientId(clientId);
+
+        assertThat(foundClients, hasSize(1));
+
+        return foundClients.get(0);
+    }
+
+    /**
+     * @param id (not client-id)
+     */
+    private String getClientSecret(String id) {
+        CredentialRepresentation secret = keycloakProvider.get()
+                .realm(REALM_NAME)
+                .clients().get(id).getSecret();
+
+        return secret.getValue();
+    }
+
+    private void doImport(String realmImport) {
+        RealmImport foundImport = getImport(realmImport);
+        realmImportService.doImport(foundImport);
+    }
+
+    private RealmImport getImport(String importName) {
+        Map<String, RealmImport> realmImports = keycloakImport.getRealmImports();
+
+        return realmImports.entrySet()
+                .stream()
+                .filter(e -> e.getKey().equals(importName))
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .get();
+    }
+}
