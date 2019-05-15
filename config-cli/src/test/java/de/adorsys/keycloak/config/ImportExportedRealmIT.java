0565ebb7285bdba2988c486bdb72bd18ef8c96f0
Boris Skert <boris.skert@adorsys.de>
2019-05-15 09:56:50 +0200

    Add integration test to import exported JSON

---
+++ config-cli/src/test/java/de/adorsys/keycloak/config/ImportExportedRealmIT.java
@@ -0,0 +1,99 @@
+package de.adorsys.keycloak.config;
+
+import de.adorsys.keycloak.config.configuration.TestConfiguration;
+import de.adorsys.keycloak.config.model.KeycloakImport;
+import de.adorsys.keycloak.config.model.RealmImport;
+import de.adorsys.keycloak.config.service.KeycloakImportProvider;
+import de.adorsys.keycloak.config.service.KeycloakProvider;
+import de.adorsys.keycloak.config.service.RealmImportService;
+import de.adorsys.keycloak.config.util.ResourceLoader;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { TestConfiguration.class },
+                      initializers = { ConfigFileApplicationContextInitializer.class })
+@ActiveProfiles("IT")
+@DirtiesContext
+public class ImportExportedRealmIT {
+    private static final String REALM_NAME = "master";
+
+    @Autowired
+    RealmImportService realmImportService;
+
+    @Autowired
+    KeycloakImportProvider keycloakImportProvider;
+
+    @Autowired
+    KeycloakProvider keycloakProvider;
+
+    KeycloakImport keycloakImport;
+
+    @Before
+    public void setup() throws Exception {
+        File configsFolder = ResourceLoader.loadResource("import-files/exported-realm");
+        this.keycloakImport = keycloakImportProvider.readRealmImportsFromDirectory(configsFolder);
+    }
+
+    @After
+    public void cleanup() throws Exception {
+        keycloakProvider.close();
+    }
+
+    @Test
+    public void shouldReadImports() {
+        assertThat(keycloakImport, is(not(nullValue())));
+    }
+
+    @Test
+    public void integrationTests() throws Exception {
+        shouldImportExportedRealm();
+    }
+
+    private void shouldImportExportedRealm() throws Exception {
+        doImport("master-realm.json");
+
+        RealmRepresentation updatedRealm = keycloakProvider.get().realm(REALM_NAME).toRepresentation();
+
+        assertThat(updatedRealm.getRealm(), is(REALM_NAME));
+        assertThat(updatedRealm.isEnabled(), is(true));
+        assertThat(updatedRealm.getLoginTheme(), is(nullValue()));
+        assertThat(
+                updatedRealm.getAttributes().get("de.adorsys.keycloak.config.import-checksum"),
+                is("8d0b874ecd38b3644dedad57113433049d5a8d97ca8832a9da76d5dd4c60eab163cfbd377612566cf166aa3cb1905fc60bf0203bd1c50447a7d09a162d385eab")
+        );
+    }
+
+    private void doImport(String realmImport) {
+        RealmImport foundImport = getImport(realmImport);
+        realmImportService.doImport(foundImport);
+    }
+
+    private RealmImport getImport(String importName) {
+        Map<String, RealmImport> realmImports = keycloakImport.getRealmImports();
+
+        return realmImports.entrySet()
+                .stream()
+                .filter(e -> e.getKey().equals(importName))
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .get();
+    }
+}
