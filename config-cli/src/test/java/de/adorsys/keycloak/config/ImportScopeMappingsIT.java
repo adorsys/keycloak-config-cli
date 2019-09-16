11403586f08db73ac43f556dfa9968c80f3e9cb4
Boris Skert <boris.skert@adorsys.de>
2019-09-16 09:43:49 +0200

    Introduce tests to reproduce the issue

---
+++ config-cli/src/test/java/de/adorsys/keycloak/config/ImportScopeMappingsIT.java
@@ -0,0 +1,177 @@
+package de.adorsys.keycloak.config;
+
+import de.adorsys.keycloak.config.configuration.TestConfiguration;
+import de.adorsys.keycloak.config.model.KeycloakImport;
+import de.adorsys.keycloak.config.model.RealmImport;
+import de.adorsys.keycloak.config.service.KeycloakImportProvider;
+import de.adorsys.keycloak.config.service.KeycloakProvider;
+import de.adorsys.keycloak.config.service.RealmImportService;
+import de.adorsys.keycloak.config.util.KeycloakAuthentication;
+import de.adorsys.keycloak.config.util.KeycloakRepository;
+import de.adorsys.keycloak.config.util.ResourceLoader;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.RoleResource;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.ScopeMappingRepresentation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(
+        classes = { TestConfiguration.class },
+        initializers = { ConfigFileApplicationContextInitializer.class }
+)
+@ActiveProfiles("IT")
+@DirtiesContext
+public class ImportScopeMappingsIT {
+    private static final String REALM_NAME = "realmWithScopeMappings";
+
+    @Autowired
+    RealmImportService realmImportService;
+
+    @Autowired
+    KeycloakImportProvider keycloakImportProvider;
+
+    @Autowired
+    KeycloakProvider keycloakProvider;
+
+    @Autowired
+    KeycloakRepository keycloakRepository;
+
+    @Autowired
+    KeycloakAuthentication keycloakAuthentication;
+
+    KeycloakImport keycloakImport;
+
+    @Before
+    public void setup() throws Exception {
+        File configsFolder = ResourceLoader.loadResource("import-files/scope-mappings");
+        this.keycloakImport = keycloakImportProvider.readRealmImportsFromDirectory(configsFolder);
+    }
+
+    @After
+    public void cleanup() throws Exception {
+        keycloakProvider.close();
+    }
+
+    @Test
+    public void shouldReadImports() {
+        assertThat(keycloakImport, is(not(nullValue())));
+    }
+
+    @Test
+    public void integrationTests() throws Exception {
+        shouldCreateRealmWithScopeMappings();
+        shouldUpdateRealmByAddingRoleToScopeMapping();
+    }
+
+    private void shouldCreateRealmWithScopeMappings() throws Exception {
+        doImport("0_create-realm-with-scope-mappings.json");
+
+        RealmRepresentation createdRealm = keycloakProvider.get().realm(REALM_NAME).partialExport(true, true);
+
+        assertThat(createdRealm.getRealm(), is(REALM_NAME));
+        assertThat(createdRealm.isEnabled(), is(true));
+
+        ScopeMappingRepresentation scopeMapping = findScopeMappingForClient(createdRealm, "scope-mapping-client");
+        assertThat(scopeMapping.getClient(), is(equalTo("scope-mapping-client")));
+
+        Set<String> scopeMappingRoles = scopeMapping.getRoles();
+        assertThat(scopeMappingRoles, hasSize(1));
+        assertThat(scopeMappingRoles, contains("scope-mapping-role"));
+    }
+
+    private void shouldUpdateRealmByAddingRoleToScopeMapping() throws Exception {
+        doImport("1_update-realm__add-role-to-scope-mapping.json");
+
+        RealmRepresentation realm = keycloakProvider.get().realm(REALM_NAME).partialExport(true, true);
+
+        assertThat(realm.getRealm(), is(REALM_NAME));
+        assertThat(realm.isEnabled(), is(true));
+
+        ScopeMappingRepresentation scopeMapping = findScopeMappingForClient(realm, "scope-mapping-client");
+        assertThat(scopeMapping.getClient(), is(equalTo("scope-mapping-client")));
+
+        Set<String> scopeMappingRoles = scopeMapping.getRoles();
+        assertThat(scopeMappingRoles, hasSize(2));
+        assertThat(scopeMappingRoles, contains("scope-mapping-role"));
+        assertThat(scopeMappingRoles, contains("added-scope-mapping-role"));
+    }
+
+
+
+    private ScopeMappingRepresentation findScopeMappingForClient(RealmRepresentation realm, String client) {
+        return realm.getScopeMappings()
+                .stream()
+                .filter(scopeMapping -> scopeMapping.getClient().equals(client))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Cannot find scope-mapping for client" + client));
+    }
+
+    private RoleRepresentation getRealmRole(String roleName) {
+        RoleResource roleResource = keycloakProvider.get()
+                .realm(REALM_NAME)
+                .roles()
+                .get(roleName);
+
+        return roleResource.toRepresentation();
+    }
+
+    private RoleRepresentation getClientRole(String clientId, String roleName) {
+        RealmResource realmResource = keycloakProvider.get()
+                .realm(REALM_NAME);
+
+        List<ClientRepresentation> clients = realmResource
+                .clients()
+                .findByClientId(clientId);
+
+        assertThat("Cannot find client by client-id", clients, hasSize(1));
+        ClientRepresentation foundClient = clients.get(0);
+
+        RoleResource roleResource = realmResource
+                .clients()
+                .get(foundClient.getId())
+                .roles()
+                .get(roleName);
+
+        return roleResource.toRepresentation();
+    }
+
+    private void doImport(String realmImport) {
+        RealmImport foundImport = getImport(realmImport);
+        realmImportService.doImport(foundImport);
+    }
+
+    private RealmImport getImport(String importName) {
+        Map<String, RealmImport> realmImports = keycloakImport.getRealmImports();
+
+        return realmImports.entrySet()
+                .stream()
+                .filter(e -> e.getKey().equals(importName))
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .get();
+    }
+}
