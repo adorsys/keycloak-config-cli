3b3be42ec324dcdd3987b37f6c2f68b9de481a3e
Thomas Wiesner <twi@adorsys.de>
2020-03-09 10:48:11 +0100

    Fixed issue-51 using a re-check for correct username (added test)

--- config-cli/src/test/java/de/adorsys/keycloak/config/ImportUsersIT.java
+++ config-cli/src/test/java/de/adorsys/keycloak/config/ImportUsersIT.java
@@ -31,7 +31,7 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.nullValue;
-import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(
@@ -81,41 +81,8 @@ public class ImportUsersIT {
         shouldCreateRealmWithUser();
         shouldUpdateRealmWithAddingClientUser();
         shouldUpdateRealmWithChangedClientUserPassword();
-    }
-
-    private void shouldCreateRealmWithUsersAndUpdateSingleUserCorrect() {
-        // arrange -> import realm with all users
-        final String realmName = "realmWithUserToChange";
-
-        doImport("3_create_realm_with_users_to_check_update.json");
-
-        RealmResource realmResource = keycloakProvider.get().realm(realmName);
-        final RealmRepresentation createdRealm = realmResource.toRepresentation();
-        assertThat(createdRealm.getRealm(), is(realmName));
-        assertThat(createdRealm.isEnabled(), is(true));
-        assertThat(realmResource.users().list().size(), is(3));
-
-        // act -> update realm with a single user to change
-        doImport("3_1_create_realm_with_users_to_check_update.json");
-
-        realmResource = keycloakProvider.get().realm(realmName);
-        assertThat(realmResource.users().list().size(), is(3));
-
-        // assert -> check whether only the "user1" was updated or not
-        final UserRepresentation updatedUser = keycloakRepository.getUser(realmName, "user");
-        assertThat(updatedUser.getEmail(), is("user@mail.de"));
-        assertThat(updatedUser.getLastName(), is("lastName"));
-        assertThat(updatedUser.getFirstName(), is("firstName"));
-
-        final UserRepresentation updatedUser1 = keycloakRepository.getUser(realmName, "user1");
-        assertThat(updatedUser1.getEmail(), is("user1@mail.de"));
-        assertThat(updatedUser1.getLastName(), is("lastName1-updated"));
-        assertThat(updatedUser1.getFirstName(), is("firstName1-updated"));
-
-        final UserRepresentation updatedUser2 = keycloakRepository.getUser(realmName, "user2");
-        assertThat(updatedUser2.getEmail(), is("user2@mail.de"));
-        assertThat(updatedUser2.getLastName(), is("lastName2"));
-        assertThat(updatedUser2.getFirstName(), is("firstName2"));
+        shouldUpdateRealmWithUserThatUsernameMatchExisting();
+        shouldCreateRealmWithUsersAndUpdateSingleUserCorrect();
     }
 
     private void shouldCreateRealmWithUser() {
@@ -210,7 +177,9 @@ public class ImportUsersIT {
         assertThat(token.getTokenType(), is("bearer"));
     }
 
-    // https://github.com/adorsys/keycloak-config-cli/issues/51
+    /**
+     * https://github.com/adorsys/keycloak-config-cli/issues/51
+     */
     private void shouldUpdateRealmWithUserThatUsernameMatchExisting() {
         doImport("3_update_realm_with_new_user.json");
 
@@ -234,6 +203,44 @@ public class ImportUsersIT {
         assertThat(createdUser.isEnabled(), is(true));
         assertThat(createdUser.getFirstName(), is("My firstname"));
         assertThat(createdUser.getLastName(), is("My lastname"));
+    }
+
+    /**
+     * https://github.com/adorsys/keycloak-config-cli/issues/51
+     */
+    private void shouldCreateRealmWithUsersAndUpdateSingleUserCorrect() {
+        // arrange -> import realm with all users
+        final String realmName = "realmWithUserToChange";
+
+        doImport("3_create_realm_with_users_to_check_update.json");
+
+        RealmResource realmResource = keycloakProvider.get().realm(realmName);
+        final RealmRepresentation createdRealm = realmResource.toRepresentation();
+        assertThat(createdRealm.getRealm(), is(realmName));
+        assertThat(createdRealm.isEnabled(), is(true));
+        assertThat(realmResource.users().list().size(), is(3));
+
+        // act -> update realm with a single user to change
+        doImport("3_1_create_realm_with_users_to_check_update.json");
+
+        realmResource = keycloakProvider.get().realm(realmName);
+        assertThat(realmResource.users().list().size(), is(3));
+
+        // assert -> check whether only the "user1" was updated or not
+        final UserRepresentation updatedUser = keycloakRepository.getUser(realmName, "user");
+        assertThat(updatedUser.getEmail(), is("user@mail.de"));
+        assertThat(updatedUser.getLastName(), is("lastName"));
+        assertThat(updatedUser.getFirstName(), is("firstName"));
+
+        final UserRepresentation updatedUser1 = keycloakRepository.getUser(realmName, "user1");
+        assertThat(updatedUser1.getEmail(), is("user1@mail.de"));
+        assertThat(updatedUser1.getLastName(), is("lastName1-updated"));
+        assertThat(updatedUser1.getFirstName(), is("firstName1-updated"));
+
+        final UserRepresentation updatedUser2 = keycloakRepository.getUser(realmName, "user2");
+        assertThat(updatedUser2.getEmail(), is("user2@mail.de"));
+        assertThat(updatedUser2.getLastName(), is("lastName2"));
+        assertThat(updatedUser2.getFirstName(), is("firstName2"));
     }
 
     private void doImport(String realmImport) {
