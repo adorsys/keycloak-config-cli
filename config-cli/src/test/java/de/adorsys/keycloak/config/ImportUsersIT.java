216228fe3f0c6ed56b64dd144e30f7ffd2daae1a
Thomas Wiesner <twi@adorsys.de>
2020-03-09 09:42:06 +0100

    Fixed issue-51 using a re-check for correct username (added test)

--- config-cli/src/test/java/de/adorsys/keycloak/config/ImportUsersIT.java
+++ config-cli/src/test/java/de/adorsys/keycloak/config/ImportUsersIT.java
@@ -9,11 +9,13 @@ import de.adorsys.keycloak.config.service.RealmImportService;
 import de.adorsys.keycloak.config.util.KeycloakAuthentication;
 import de.adorsys.keycloak.config.util.KeycloakRepository;
 import de.adorsys.keycloak.config.util.ResourceLoader;
+import java.util.List;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -84,6 +86,42 @@ public class ImportUsersIT {
         shouldCreateRealmWithUser();
         shouldUpdateRealmWithAddingClientUser();
         shouldUpdateRealmWithChangedClientUserPassword();
+        shouldCreateRealmWithUsersAndUpdateSingleUserCorrect();
+    }
+
+    private void shouldCreateRealmWithUsersAndUpdateSingleUserCorrect() throws Exception {
+        // arrange -> import realm with all users
+        final String realmName = "realmWithUserToChange";
+
+        doImport("3_create_realm_with_users_to_check_update.json");
+
+        RealmResource realmResource = keycloakProvider.get().realm(realmName);
+        final RealmRepresentation createdRealm = realmResource.toRepresentation();
+        assertThat(createdRealm.getRealm(), is(realmName));
+        assertThat(createdRealm.isEnabled(), is(true));
+        assertThat(realmResource.users().list().size(), is(3));
+
+        // act -> update realm with a single user to change
+        doImport("3_1_create_realm_with_users_to_check_update.json");
+
+        realmResource = keycloakProvider.get().realm(realmName);
+        assertThat(realmResource.users().list().size(), is(3));
+
+        // assert -> check whether only the "user1" was updated or not
+        final UserRepresentation updatedUser = keycloakRepository.getUser(realmName, "user");
+        assertThat(updatedUser.getEmail(), is("user@mail.de"));
+        assertThat(updatedUser.getLastName(), is("lastName"));
+        assertThat(updatedUser.getFirstName(), is("firstName"));
+
+        final UserRepresentation updatedUser1 = keycloakRepository.getUser(realmName, "user1");
+        assertThat(updatedUser1.getEmail(), is("user1@mail.de"));
+        assertThat(updatedUser1.getLastName(), is("lastName1-updated"));
+        assertThat(updatedUser1.getFirstName(), is("firstName1-updated"));
+
+        final UserRepresentation updatedUser2 = keycloakRepository.getUser(realmName, "user2");
+        assertThat(updatedUser2.getEmail(), is("user2@mail.de"));
+        assertThat(updatedUser2.getLastName(), is("lastName2"));
+        assertThat(updatedUser2.getFirstName(), is("firstName2"));
     }
 
     private void shouldCreateRealmWithUser() throws Exception {
