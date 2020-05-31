fb694993d31c357f7253f65ae87ac567f1988722
Jan-Otto Kröpke <mail@jkroepke.de>
2020-05-31 16:23:52 +0200

    Update test-with-import-files.sh

--- contrib/native/test-with-import-files.sh
+++ contrib/native/test-with-import-files.sh
@@ -9,7 +9,7 @@ export SPRING_PROFILES_ACTIVE=dev
 ./target/keycloak-config-cli-native --import.force=true
 
 while read -r file; do
-  ./target/de.adorsys.keycloak.config.keycloakconfigapplication --import.path="${file}"
+  ./target/keycloak-config-cli-native --import.path="${file}"
 done < <(
   find src/test/resources/import-files \
     -type f \
