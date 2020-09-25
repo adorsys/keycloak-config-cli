b8869fab8ee27b182a97f317fe2f2363a19e3553
Jan-Otto Kröpke <joe@adorsys.de>
2020-09-25 14:51:06 +0200

    Add workaround

--- src/main/java/de/adorsys/keycloak/config/configuration/KeycloakConfigConfiguration.java
+++ src/main/java/de/adorsys/keycloak/config/configuration/KeycloakConfigConfiguration.java
@@ -27,7 +27,7 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProce
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class KeycloakConfigConfiguration {
     @Bean
     public static BeanDefinitionRegistryPostProcessor ripper() {
