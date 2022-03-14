2c6315c5b5077b7bd56b70adb147f7c3f76076ae
Dusan Jakub <dusan.jakub@ysoft.com>
2022-03-14 16:08:59 +0100

    Simplification of IdpPermissionResolver

--- src/main/java/de/adorsys/keycloak/config/service/clientauthorization/IdpPermissionResolver.java
+++ src/main/java/de/adorsys/keycloak/config/service/clientauthorization/IdpPermissionResolver.java
@@ -21,24 +21,21 @@
 package de.adorsys.keycloak.config.service.clientauthorization;
 
 import de.adorsys.keycloak.config.exception.ImportProcessingException;
-import de.adorsys.keycloak.config.exception.KeycloakRepositoryException;
 import de.adorsys.keycloak.config.repository.IdentityProviderRepository;
 import org.keycloak.representations.idm.IdentityProviderRepresentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import javax.ws.rs.NotFoundException;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class IdpPermissionResolver implements PermissionResolver {
     private static final Logger logger = LoggerFactory.getLogger(IdpPermissionResolver.class);
 
     private final String realmName;
     private final IdentityProviderRepository identityProviderRepository;
-    private final Map<String, String> idpInternalIdToAlias = new HashMap<>();
-    private final Map<String, String> idpAliasToInternalId = new HashMap<>();
+    private List<IdentityProviderRepresentation> identityProviders;
 
     public IdpPermissionResolver(String realmName, IdentityProviderRepository identityProviderRepository) {
         this.realmName = realmName;
@@ -47,49 +44,33 @@ public class IdpPermissionResolver implements PermissionResolver {
 
     @Override
     public String resolveObjectId(String alias, String authzName) {
-        if (idpAliasToInternalId.containsKey(alias)) {
-            return idpAliasToInternalId.get(alias);
-        }
-
-        try {
-            IdentityProviderRepresentation provider = identityProviderRepository.getByAlias(realmName, alias);
-            if (provider == null) {
-                throw new NotFoundException();
-            }
-            idpInternalIdToAlias.put(provider.getInternalId(), provider.getAlias());
-            idpAliasToInternalId.put(provider.getAlias(), provider.getInternalId());
-            return provider.getInternalId();
-        } catch (NotFoundException | KeycloakRepositoryException e) {
-            throw new ImportProcessingException("Cannot find identity provider with alias '%s' in realm '%s' for '%s'", alias, realmName, authzName);
-        }
+        return getIdentityProviders()
+                .filter(idp -> Objects.equals(idp.getAlias(), alias))
+                .map(IdentityProviderRepresentation::getInternalId)
+                .findAny()
+                .orElseThrow(() -> new ImportProcessingException(
+                        "Cannot find identity provider with alias '%s' in realm '%s' for '%s'", alias, realmName, authzName));
     }
 
     @Override
     public void enablePermissions(String id) {
-        String alias = getAliasFromIdentityProviderInternalId(id);
+        String alias = getIdentityProviders()
+                .filter(idp -> Objects.equals(idp.getInternalId(), id))
+                .map(IdentityProviderRepresentation::getAlias)
+                .findAny()
+                .orElseThrow(() -> new ImportProcessingException(
+                        "Cannot find identity provider with internal id '%s' in realm '%s'", id, realmName));
+
         if (!identityProviderRepository.isPermissionEnabled(realmName, alias)) {
             logger.debug("Enable permissions for Identity Provider '{}' in realm '{}'", id, realmName);
             identityProviderRepository.enablePermission(realmName, alias);
         }
     }
 
-    private String getAliasFromIdentityProviderInternalId(String internalId) {
-        if (idpInternalIdToAlias.containsKey(internalId)) {
-            return idpInternalIdToAlias.get(internalId);
+    private Stream<IdentityProviderRepresentation> getIdentityProviders() {
+        if (identityProviders == null) {
+            identityProviders = identityProviderRepository.getAll(realmName);
         }
-
-        try {
-            List<IdentityProviderRepresentation> providers = identityProviderRepository.getAll(realmName);
-            for (IdentityProviderRepresentation provider : providers) {
-                idpInternalIdToAlias.put(provider.getInternalId(), provider.getAlias());
-                idpAliasToInternalId.put(provider.getAlias(), provider.getInternalId());
-            }
-            if (idpInternalIdToAlias.containsKey(internalId)) {
-                return idpInternalIdToAlias.get(internalId);
-            }
-            throw new NotFoundException();
-        } catch (NotFoundException | KeycloakRepositoryException e) {
-            throw new ImportProcessingException("Cannot find identity provider with internal id '%s' in realm '%s'", internalId, realmName);
-        }
+        return identityProviders.stream();
     }
 }
