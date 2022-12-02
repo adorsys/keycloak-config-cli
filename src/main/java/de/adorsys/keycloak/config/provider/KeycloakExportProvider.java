04ed90bfbd168f6632b47b4330fdac86cff7daa8
Simon Levermann <simon.levermann@codecentric.de>
2022-12-02 14:50:39 +0100

    Fix checkstyle warnings

--- src/main/java/de/adorsys/keycloak/config/provider/KeycloakExportProvider.java
+++ src/main/java/de/adorsys/keycloak/config/provider/KeycloakExportProvider.java
@@ -159,7 +159,8 @@ public class KeycloakExportProvider {
             return false;
         }
 
-        if (!this.normalizationConfigProperties.getFiles().isIncludeHiddenFiles() && (file.isHidden() || FileUtils.hasHiddenAncestorDirectory(file))) {
+        if (!this.normalizationConfigProperties.getFiles().isIncludeHiddenFiles()
+                && (file.isHidden() || FileUtils.hasHiddenAncestorDirectory(file))) {
             return false;
         }
 
