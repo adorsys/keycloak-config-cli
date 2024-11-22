27b6573cd5d3909cadab2c08280e4247ffa1c425
Thomas Darimont <thomas.darimont@googlemail.com>
2024-11-22 09:09:31 +0100

    Add missing ConditionalOnProperty to OtpPolicyImportService and OtpPolicyRepository

--- src/main/java/de/adorsys/keycloak/config/repository/OtpPolicyRepository.java
+++ src/main/java/de/adorsys/keycloak/config/repository/OtpPolicyRepository.java
@@ -22,9 +22,11 @@ package de.adorsys.keycloak.config.repository;
 
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnProperty(prefix = "run", name = "operation", havingValue = "IMPORT", matchIfMissing = true)
 public class OtpPolicyRepository {
     private final RealmRepository realmRepository;
 
