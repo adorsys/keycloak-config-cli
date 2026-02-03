575035cc4a1b5e15ff8bf53bfbd76ad8e814ec58
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2026-02-03 15:47:30 +0100

    Add OrganizationImporterFactory for creating organization importers based on Keycloak version

---
+++ src/main/java/de/adorsys/keycloak/config/service/organization/OrganizationImporterFactory.java
@@ -0,0 +1,49 @@
+/*-
+ * ---license-start
+ * keycloak-config-cli
+ * ---
+ * Copyright (C) 2017 - 2021 adorsys GmbH & Co. KG @ https://adorsys.com
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+
+package de.adorsys.keycloak.config.service.organization;
+
+import de.adorsys.keycloak.config.condition.ConditionalOnKeycloakVersion26OrNewer;
+import de.adorsys.keycloak.config.condition.ConditionalOnKeycloakVersionOlderThan26;
+import de.adorsys.keycloak.config.properties.ImportConfigProperties;
+import de.adorsys.keycloak.config.repository.OrganizationRepository;
+import de.adorsys.keycloak.config.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrganizationImporterFactory {
+
+    @Bean
+    @ConditionalOnKeycloakVersion26OrNewer
+    public OrganizationImporter createDefaultImporter(
+            OrganizationRepository organizationRepository,
+            UserRepository userRepository,
+            ImportConfigProperties importConfigProperties
+    ) {
+        return new DefaultOrganizationImporter(organizationRepository, userRepository, importConfigProperties);
+    }
+
+    @Bean
+    @ConditionalOnKeycloakVersionOlderThan26
+    public OrganizationImporter createLegacyImporter() {
+        return new LegacyOrganizationImporter();
+    }
+}
